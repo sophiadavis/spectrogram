c247a010e6203e8f2bf4f9d8146572c4e5718346
Sophia Davis <scdgrapefruit@gmail.com>
2013-10-30 23:48:50 -0500

    basic functionality, hopefully, trying to graph Spectrogram

--- spectogram.py
+++ spectogram.py
@@ -2,68 +2,83 @@ import wave
 from graphics import *
 import numpy
 import struct
+import math
 
 def main():
-    speech = wave.open("si762.wav")
-    print "number of audio channels: "
-    print speech.getnchannels()
-    print "sample width in bytes: "
-    print speech.getsampwidth()
-    print "number of audio frames: "
+#     speech = wave.open("si762.wav")
+#     nframes = speech.getnframes()
+#     
+#     window = GraphWin("Speech", 1000, 500)
+#     window.setCoords(0, -10000, nframes, 10000) #setCoords(xll, yll, xur, yur)
+#     
+#     by = 250
+#     for i in range(0, nframes/by):
+#         speech.readframes(by-1)
+#         raw = speech.readframes(1)
+#         converted = struct.unpack("<h", raw)
+#         frame = int(converted[0])
+#         point1 = Point(i*by, 0) 
+#         point2 = Point(i*by + .8, frame)
+#         rect = Rectangle(point1, point2)
+#         rect.draw(window)
+#     window.getMouse()
+#     window.close()
+#     speech.close()
+    
+    speech = wave.open("bowl.wav")
     nframes = speech.getnframes()
-    print nframes 
-    print "frame rate: "
-    print speech.getframerate()
-    print "compression type: "
-    print speech.getcomptype()
-    print speech.getcompname()
-#    speech.rewind() # rewinds file pointer to beginning of audio stream
-    window = GraphWin("Speech", 1000, 500)
-    window.setCoords(0, -10000, nframes, 10000) #setCoords(xll, yll, xur, yur)
-#     line = Line(Point(0, 0), Point(4500, 0))
-#     line.setOutline("green")
-#     line.setFill("green")
-#     line.draw(window)
-#     for i in range(0, 200):#nframes):
-    by = 250
-    for i in range(0, nframes/by):
-        speech.readframes(by-1)
-        raw = speech.readframes(1)
-#         print "raw " + raw
-        converted = struct.unpack("<h", raw)
-#         print "converted: "
-#         print converted
-        print "i: " + str(i*by) 
-        frame = int(converted[0])
+    window = 25.0
+    sampling_rate = speech.getframerate()
+    samps_per_window = int(sampling_rate*(window/1000))
+    nyquist = samps_per_window/2
+    
+    time_steps = {}
+    window_start = 0
+    while window_start < (nframes - samps_per_window):
+        samples = []
         
-        print frame
-        point1 = Point(i*by, 0) 
-        point2 = Point(i*by + .8, frame)
-#         point1.draw(window)
-#         point2.draw(window)
-#         point1 = Point(i, 0) 
-#         point2 = Point((i+1), 20)
-        rect = Rectangle(point1, point2)
-#         rect = Rectangle(Point(i, 0), Point(i+.8, 5))
-#         rect.setFill("green")
-        rect.draw(window)
-    window.getMouse() # pause for click in window
-    window.close()
+        speech.readframes(window_start) # catch up to correct location
+        
+        for i in range(0, samps_per_window):
+            raw = speech.readframes(1)
+            converted = struct.unpack("<h", raw)
+            samples = samples + [int(converted[0])]
+        samples_array = numpy.array(samples)
+        dft_matrix = numpy.fft.fft(samples_array)
 
-#     for speech.readframes(10) # reads and returns at most n frames of audio, as a string of bytes
+        magnitudes = []
+        for i in range(0, nyquist):
+            real = numpy.real(dft_matrix[i])
+            imag = numpy.imag(dft_matrix[i])
+            sq_mag = math.sqrt(math.pow(real, 2) + math.pow(real, 2))
+            mag = 2*sq_mag/samps_per_window
+            log_mag = 10*math.log10(mag)
+            magnitudes = magnitudes + [log_mag]
+        time_steps[window_start] = magnitudes
+        window_start += 10
+        speech.rewind()
     
-    
+    window = GraphWin("Spectrograph", 1000, 500)
+    window.setCoords(0, -50, time_steps, 50) #setCoords(xll, yll, xur, yur)
+    for i in time_steps:
+#         print max(time_steps[i])
+        point1 = Point(i, 0)
+        point2 = Point(i+1, nyquist)
+        
+        window.getMouse()
+        window.close()
+# A black-and-white image, with time along the x-axis, 
+# (where each 10ms window is 1 pixel of width), 
+# frequency along the y-axis and 
+# intensity indicated by the darkness of the pixel, 
+#         with white meaning 0 intensity and 
+#         black meaning maximum intensity.
 
+
+#     
     
     
     speech.close() 
     
-#     win = GraphWin("Window", 200, 200)
-#     msg = Text(Point(100, 50), 'Original message...')
-#     msg.draw(win)
-#     # ...
-#     # ... just important that there is a drawn Text object
-#     win.promptClose(msg)
-
 if __name__ == "__main__":
     main()
